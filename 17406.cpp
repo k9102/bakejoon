e20d178077a926a77129f755f96c2aa3e6cf72ad
gary kim <gary.kimm@gmail.com>
2019-10-10 16:34:59 +0900

    second try

--- 17406.cpp
+++ 17406.cpp
@@ -10,10 +10,11 @@
 using namespace std;
 
 int N_, M_, K_;
-int mat_[50][50];
+array<array<int,50>,50> mat_;
 int rcs_[6][3];
 
 int dir_[4][2] = { {0,1},{1,0},{0,-1},{-1,0} };
+int mi_ = INT_MAX;
 
 int main()
 {
@@ -30,34 +31,43 @@ int main()
 			cin >> rcs_[r][c];
 		}
 
-	for (int k = 0; k < K_; k++)
-	{
-		auto[r, c, s] = rcs_[k]; r--, c--;
-		
-		for (int sl = 1; sl <= s; sl++)
-		{
-			auto[cr, cc,cv] = tuple(r-sl,c-sl, mat_[r-sl][c-sl]);
-			for (int d = 0; d < 4; d++)
-			{
-				auto[ir, ic] = dir_[d];
-				for (int i = 0; i < 2*sl; i++)
-				{			
-					auto[nr, nc, nv] = tuple(cr + ir, cc + ic, mat_[cr + ir][cc + ic]);
-					
-					mat_[nr][nc] = cv;
+	int seq[] = {0,1,2,3,4,5 };
 
-					tie(cr, cc, cv) = tuple(nr, nc, nv);
+	array<array<int, 50>, 50> mat;
+	do {
+		 mat = mat_;
+		for (int k = 0; k < K_; k++)
+		{
+			auto[r, c, s] = rcs_[seq[k]]; r--, c--;
+
+			for (int sl = 1; sl <= s; sl++)
+			{
+				auto[cr, cc, cv] = tuple(r - sl, c - sl, mat[r - sl][c - sl]);
+				for (int d = 0; d < 4; d++)
+				{
+					auto[ir, ic] = dir_[d];
+					for (int i = 0; i < 2 * sl; i++)
+					{
+						auto[nr, nc, nv] = tuple(cr + ir, cc + ic, mat[cr + ir][cc + ic]);
+
+						mat[nr][nc] = cv;
+
+						tie(cr, cc, cv) = tuple(nr, nc, nv);
+					}
 				}
 			}
 		}
-	}
 
-	int mi = INT_MAX;
-	for (int r = 0; r < N_; r++)
-	{
-		mi = min(mi,accumulate(mat_[r], mat_[r] + M_, 0));
-	}
+		int mi = INT_MAX;
+		for (int r = 0; r < N_; r++)
+		{
+			mi = min(mi, accumulate(begin(mat[r]),end(mat[r]),0));
+		}
 
-	cout << mi << endl;
+		mi_ = min(mi_, mi);
+
+	} while (next_permutation(seq, seq + K_));
+
+	cout << mi_ << endl;
 	return 0;
 }
