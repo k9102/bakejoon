c150ac165193a92fb2716a22aa2713f6e99aba33
gary kim <gary.kimm@gmail.com>
2019-10-11 09:52:34 +0900

    17471 source code is organized

--- 17471.cpp
+++ 17471.cpp
@@ -12,22 +12,17 @@ using namespace std;
 int N_;
 int pop_[10];
 vector<vector<int>> adj_;
-array<bool, 10> tbl_;
-array<bool, 10> visited_;
+array<bool, 10> com_;
+array<bool, 10> visit_;
 bool flag_;
-int mi_ = INT_MAX;
+int min_ = INT_MAX;
 
 void traverse(int loc)
 {
-	visited_[loc] = true;
+	visit_[loc] = true;
 
-	for (auto n : adj_[loc])
-	{
-		if (tbl_[n] == flag_ && !visited_[n])
-		{
-			traverse(n);
-		}
-	}
+	for (auto n : adj_[loc])  if (com_[n] == flag_ && !visit_[n]) traverse(n);
+	
 }
 
 int main()
@@ -39,7 +34,7 @@ int main()
 	for (int i = 0; i < N_; i++) {
 		int n; cin >> n;
 		vector<int> vec;
-		for (int j = 0; j < n; j++) { 
+		for (int j = 0; j < n; j++) {
 			int v; cin >> v; vec.push_back(v-1);
 		}
 		adj_.push_back(move(vec));
@@ -48,36 +43,32 @@ int main()
 	
 	for (int g = 1; g <= N_/2; g++)
 	{
-		tbl_[N_- g] = true;
+		com_[N_- g] = true;
 
 		do {
-			fill_n(begin(visited_), N_,false);
+			fill_n(begin(visit_), N_,false);
 			
 			for (auto flag : {true,false})
 			for(int loc=0;loc<N_;loc++)
-				if (tbl_[loc] == flag) {
+				if (com_[loc] == flag) {
 					flag_ = flag;
 					traverse(loc);
 					break;
 				}
 
-			bool ret = all_of(begin(visited_), begin(visited_)+N_, [](auto v) { return v == true; });
-			if (ret)
+			if (bool r = all_of(begin(visit_), begin(visit_) + N_, [](auto v) { return v == true; });r)
 			{
 				auto[tsum, fsum] = tuple(0, 0);
-				for (int i = 0; i < N_; i++)
-				{
-					if (tbl_[i]) tsum += pop_[i]; else fsum += pop_[i];
-				}
+				for (int i = 0; i < N_; i++) if (com_[i]) tsum += pop_[i]; else fsum += pop_[i];
 				auto[a, b] = minmax(tsum, fsum);
-				mi_ = min(mi_, b - a);
+				min_ = min(min_, b - a);
 			}
-		} while (next_permutation(begin(tbl_), begin(tbl_)+N_));
+		} while (next_permutation(begin(com_), begin(com_)+N_));
 		
 	}
 
-	if (mi_ == INT_MAX) mi_ = -1;
-	cout << mi_ << endl;
+	if (min_ == INT_MAX) min_ = -1;
+	cout << min_ << endl;
 
 	return 0;
 }
