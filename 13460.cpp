d3f6fbc79b5e573cbf81e00d730cb5e8ff0a2b52
김규열(k9102) <k9102@lginnotek.com>
2019-10-12 11:53:15 +0900

    13460 backup

---
+++ 13460.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <algorithm>
+#include <numeric>
+#include <limits.h>
+#include <array>
+
+using namespace std;
+
+int n_, m_;
+char mat_[10][11];
+
+
+int main()
+{
+	cin >> n_ >> m_;
+	
+	for (int r = 0; r < n_; r++)
+	{
+		cin >> mat_[r];
+	}
+
+
+	return 0;
+}
