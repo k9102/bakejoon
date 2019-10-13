42b28bd841084d0c6afb3a27b63becf03e86a01e
김규열(k9102) <k9102@lginnotek.com>
2019-10-13 14:13:03 +0900

    13458 backup

---
+++ 13458.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <tuple>
+#include <algorithm>
+#include <numeric>
+#include <limits.h>
+#include <array>
+
+using namespace std;
+
+
+int main()
+{
+
+
+	return 0;
+}
