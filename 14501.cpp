21be7150eabdf6fe3099356c026bea3993e1b259
김규열(k9102) <k9102@lginnotek.com>
2019-10-13 20:40:50 +0900

    14501 organized

--- 14501.cpp
+++ 14501.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <tuple>
 #include <algorithm>
-#include <numeric>
 #include <limits.h>
-#include <array>
 
 using namespace std;
 
-
 int n_;
 int tp_[15][2];
-
 int max_ = INT_MIN;
 
 void Traverse(int acc,int dp)
@@ -20,18 +14,17 @@ void Traverse(int acc,int dp)
 	if (dp >= n_) 
 	{
 		if(dp==n_) max_ = max(max_, acc);
-		return;
 	}
+	else
+	{
+		auto[t, p] = tp_[dp];
 
-	auto[t, p] = tp_[dp];
-	
-	Traverse(acc + p, dp + t);
-	Traverse(acc, dp + 1);
+		Traverse(acc + p, dp + t);
+		Traverse(acc, dp + 1);
+	}
 }
 int main()
 {
-	ios_base::sync_with_stdio(false);
-
 	cin >> n_;
 	for (int i = 0; i < n_; i++)
 	{
@@ -42,5 +35,4 @@ int main()
 	Traverse(0,0);
 
 	cout << max_ << endl;
-	return 0;
 }
