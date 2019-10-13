e0602bd3b3c834fc8bcb243edbc529e1667c9458
김규열(k9102) <k9102@lginnotek.com>
2019-10-13 20:14:41 +0900

    14501 first try

--- 14501.cpp
+++ 14501.cpp
@@ -11,8 +11,23 @@ using namespace std;
 
 
 int n_;
-int tp_[1000][2];
+int tp_[15][2];
 
+int max_ = INT_MIN;
+
+void Traverse(int acc,int dp)
+{
+	if (dp >= n_) 
+	{
+		if(dp==n_) max_ = max(max_, acc);
+		return;
+	}
+
+	auto[t, p] = tp_[dp];
+	
+	Traverse(acc + p, dp + t);
+	Traverse(acc, dp + 1);
+}
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -23,5 +38,9 @@ int main()
 		auto &[t, p] = tp_[i];
 		cin >> t >> p;
 	}
+	
+	Traverse(0,0);
+
+	cout << max_ << endl;
 	return 0;
 }
