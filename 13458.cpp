a1c9e52941163d5039a5c81561e4a60c72538148
김규열(k9102) <k9102@lginnotek.com>
2019-10-13 14:34:12 +0900

    13458 first try(completed)

--- 13458.cpp
+++ 13458.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <tuple>
-#include <algorithm>
-#include <numeric>
-#include <limits.h>
-#include <array>
+
 
 using namespace std;
 
+int n_,b_,c_;
+vector<int> aj_;
 
 int main()
 {
+	cin >> n_; aj_.resize(n_);
+	for (int i = 0; i < n_; i++) cin >> aj_[i];
+	cin >> b_ >> c_;
 
+	long long cnt = n_;
+	for (auto d : aj_)
+	{
+		if (auto r = d - b_; r > 0) {
+			cnt += r / c_;
+			if (r%c_) cnt++;
+		}
 
+	}
+
+	cout << cnt << endl;
 	return 0;
 }
