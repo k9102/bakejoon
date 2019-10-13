7fbe8b9f16898fb48182301343ccbc1fbbe2ecf5
김규열(k9102) <k9102@lginnotek.com>
2019-10-13 22:10:46 +0900

    14502 first try

--- 14502.cpp
+++ 14502.cpp
@@ -11,7 +11,29 @@ using namespace std;
 
 int n_, m_;
 array<array<int,8>,8> map_;
+array<array<int, 8>, 8> tmap_;
 vector<pair<int, int>> spc_;
+vector<pair<int, int>> vrs_;
+int max_ = INT_MIN;
+
+inline bool CanGo(int r, int c)
+{
+	return (r < 0 || r >= n_ || c < 0 || c >= m_) ? false : true;
+}
+void Traverse(int r, int c)
+{
+
+	if (!CanGo(r, c) || tmap_[r][c] != 0) return;
+	
+
+	tmap_[r][c] = 2;
+
+	Traverse(r + 1, c);
+	Traverse(r - 1,c);
+	Traverse(r, c+1);
+	Traverse(r, c-1);
+}
+
 int main()
 {
 	cin >> n_ >> m_;
@@ -21,6 +43,7 @@ int main()
 			auto &d = map_[r][c];
 			cin >> d;
 			if (d == 0) spc_.push_back({ r, c });
+			if (d == 2) vrs_.push_back({ r, c });
 		}
 
 	auto sz = size(spc_);
@@ -29,15 +52,31 @@ int main()
 	comb[sz - 1] = comb[sz - 2] = comb[sz - 3] = true;
 
 	do {
-		auto tmap = map_;
-		for (int i = 0; i < sz; i++)
+		tmap_ = map_;
+		for (size_t i = 0; i < sz; i++)
 		{
 			if (comb[i]) {
 				auto[r, c] = spc_[i];
-				tmap[r][c] = 1;
+				tmap_[r][c] = 1;
 			}
 		}
 
+		for (auto[r, c] : vrs_)
+		{
+			tmap_[r][c] = 0;
+			Traverse(r, c);
+		}
+
+		int cnt = 0;
+		for (int r=0;r<n_;r++)
+			for (int c = 0; c < m_; c++)
+			{
+				if (!tmap_[r][c]) cnt++;
+			}
+	
+		max_ = max(max_, cnt);
 
 	} while (next_permutation(begin(comb), end(comb)));
+
+	cout << max_ << endl;
 }
