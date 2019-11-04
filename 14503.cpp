3075c6900c7f45c3de596e32bd6d4528f208f394
gary kim <gary.kimm@gmail.com>
2019-11-04 16:41:06 +0900

    14503 code is organized

--- 14503.cpp
+++ 14503.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
-#include <algorithm>
-#include <limits.h>
 #include <array>
 
 
 using namespace std;
 
+using uint = unsigned int;
 
-using uchar = unsigned int;
-
-int n_, m_;
-int r_, c_;
-uchar d_;
-int mv_[4][2] = { {-1,0},{0,1},{1,0},{0,-1} }; /*N E S W*/
+int n_, m_, r_, c_;
+uint d_;
+int mv_[4][2] = { {-1,0},{0,1},{1,0},{0,-1} };
 array<array<int, 50>, 50> mat_;
+
+pair<int,int> mv(int d,int r, int c)
+{
+	auto[ir, ic] = mv_[d];
+
+	r += ir;
+	c += ic;
+
+	return { r,c };
+}
 
 int main()
 {
-	int clean_cnt = 0;
+	int cnt = 0;
 	cin >> n_ >> m_;
 	cin >> r_ >> c_ >> d_;
 
@@ -27,57 +33,44 @@ int main()
 			cin >> mat_[r][c];
 		}
 
-
 	while(true) {
 
 		/*1*/
 		if (mat_[r_][c_] == 0)
 		{
-			clean_cnt++;
+			cnt++;
 			mat_[r_][c_] = 2;
 		}
 
 		/*2b*/
 		for (int i = 0; i < 4; i++)
 		{
-			uchar d = (d_-(i+1)) % 4;
-			auto[ir, ic] = mv_[d];
-			int r = r_, c = c_;
-
-			r += ir;
-			c += ic;
+			uint d = (d_-(i+1)) % 4;
+			auto[r, c] = mv(d, r_, c_);
 
 			/*2a*/
-			if (mat_[r][c] == 0)
+			if (!mat_[r][c])
 			{
-				d_ = d;
-				r_ = r;
-				c_ = c;
+				d_ = d; r_ = r; c_ = c;
 				break;
 			}
 		}
 
-		
-		if (mat_[r_][c_] != 0)
+		if (mat_[r_][c_])
 		{
-			uchar d = (d_ + 2) % 4;
-			auto[ir, ic] = mv_[d];
-			int r = r_, c = c_;
-			r += ir;
-			c += ic;
+			uint d = (d_ + 2) % 4;
+			auto[r, c] = mv(d, r_, c_);
 		
 			if (mat_[r][c] != 1)  /*2c*/
 			{
-				r_ = r;
-				c_ = c;
+				r_ = r; c_ = c;
 			}
-			else   /*2d*/
-			{
+			else { /*2d*/
 				break;
 			}
 		}
 
 	}
 
-	cout << clean_cnt << endl;
+	cout << cnt << endl;
 }
